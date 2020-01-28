7fbfba0604455bbbaacac482cca8ca4a00bc3add
mpahlabi <60122619+mpahlabi@users.noreply.github.com>
2020-01-27 20:11:19 -0500

    Update warfilecopy.sh

--- warfilecopy.sh
+++ warfilecopy.sh
@@ -1,10 +1,11 @@
 ssh -tt ec2-user@184.72.182.254 '
 rsync /var/lib/jenkins/workspace/GameOfLife/gameoflife-web/target/gameoflife.war ec2-user@54.152.111.51:/tmp
 exit
+'
 ssh -tt ec2-user@54.152.111.51 '
 chown root:tomcat /tmp/*.war
 mv /tmp/gameoflife.war /usr/share/tomcat/webapps/
-
+'
 
 
 
